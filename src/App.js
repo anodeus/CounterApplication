d26901e09a2313f6294a4558bb73a660fb4f2ff1
AbhiSingh <abhisinghkirad7@gmail.com>
2023-02-15 21:41:36 +0530

    origin flile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import React, {useState} from 'react'
+
+export default function App() {
+  const [count, setCount]= useState(0)
+  const incrementValue=()=>{
+    setCount(count => count+1)
+  }
+  const decrementValue=()=>{
+    setCount(count=>count-1)
+  }
+  const resetValue = () => {
+    setCount(0); 
+  };
+  let myStyle={
+    color: "green"
+  }
+  if(count>=5 && count<=9){
+    myStyle ={
+      color: "blue"
+    }
+  }
+  if(count===10){
+    myStyle={
+      color: "red"
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={myStyle}>Counter</h1>
+      <h1>{count}</h1>
+      <button onClick={incrementValue} disabled={count >9 ? true : false}>+</button>
+      <button onClick={decrementValue} disabled={count<1 ? true: false }>-</button>
+      <button onClick={resetValue}>Reset</button>
     </div>
-  );
+  )
 }
 
-export default App;
+// export default App;
